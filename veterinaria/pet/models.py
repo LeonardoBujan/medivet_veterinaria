a6d6ab18efdfd40e429ce881e20bc2de20d56eb8
Leonardo Bujan <tatobujan86@gmail.com>
2024-11-11 14:19:26 -0300

    actualiza aplicacion pet

--- veterinaria/pet/models.py
+++ veterinaria/pet/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import User
 from type_pet.models import TypePet
 
 class Pet(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    type_pet = models.ForeignKey(TypePet, on_delete=models.CASCADE)
-    pet_name = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    type_pet = models.ForeignKey(TypePet, on_delete=models.CASCADE, null=False)
+    pet_name = models.CharField(max_length=30, null=False)
     date_birth_pet = models.DateField()
+    image_pet = models.ImageField(upload_to='pets', null=True, blank=True)
 
     class Meta:
         db_table = 'pets'
